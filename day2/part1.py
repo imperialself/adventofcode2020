210961a5803fca6e7759f068612cccf1baf1761b
Kevin Eales Monroe <kevbot@inRainbows.local>
2020-12-01 23:41:48 -0800

    day 2

---
+++ day2/part1.py
@@ -0,0 +1,25 @@
+# find number of "valid" passwords
+
+import re
+
+with open('input') as file:
+    passwordlist = [line.rstrip('\n') for line in file]
+
+# regexes
+rLetter   = '[a-z]'
+rMin      = '^[0-9]{1,}'
+rMax      = '\-[0-9]{1,}'
+rPassword = '\:\s[a-z]{1,100}'
+
+valid = 0
+
+for x in passwordlist:
+	letter = re.search(rLetter, x).group(0)
+	min = int(re.search(rMin, x).group(0))
+	max = int(re.search(rMax, x).group(0)[1:])
+	password = re.search(rPassword, x).group(0)[2:]
+
+	if password.count(letter) >= min and password.count(letter)<= max:
+		valid += 1
+
+print(valid)
