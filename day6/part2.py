fa6817c22c7b9ffd4e41509d26202edb3aa78393
Kevin Eales Monroe <kevin.w.eales@gmail.com>
2020-12-07 10:57:45 -0800

    rename var for clarity

--- day6/part2.py
+++ day6/part2.py
@@ -11,16 +11,15 @@ yesCounts = []
 
 # Starts with whatever person 1 had, iterate through each person and remove every remaining letter not present
 def countYeses(c):
-	u = list(c[0])				# Start with whatever person 1 said yes to
+	unanimous = list(c[0])				# Start with whatever person 1 said yes to
 	for p in c:
 		for l in c[0]:					# More performant than looping through 26 letters every time
-			print(c[0],u)
 			if l not in p:
 				try:					# This is going to fail if it's already been removed so we only "try"
-					u.remove(l)
+					unanimous.remove(l)
 				except ValueError:
 					pass
-	yesCounts.append(len(u))		# The length is a count of the remaining letters
+	yesCounts.append(len(unanimous))		# The length is a count of the remaining letters
 
 # Iterate through all groups and count their yeses
 for group in customs:
