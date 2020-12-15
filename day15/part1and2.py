755fcf402b5b29942d313fd0726d1144dc9eee6a
Kevin Eales Monroe <kevin.w.eales@gmail.com>
2020-12-15 15:36:27 -0800

    day 15

---
+++ day15/part1and2.py
@@ -0,0 +1,39 @@
+# Find the nth number in series following rules about 
+
+# part 1: 2020
+# part 2: 30000000
+target = 2020 # nth iteration we want to know
+
+# Initialize
+said = {}
+starting = [2,0,1,7,4,14,18]	# The input given by AoC
+
+def takeTurn(turn):
+	global said,lastTurn
+
+	# If still in the starting numbers
+	if turn < len(starting)+1:
+		said[starting[turn-1]] = [turn]
+		lastTurn = starting[-1]
+		return
+
+	# ok, rules of the game:
+	if len(said[lastTurn]) > 1:		# If last turn's num has been said 2+ times, find diff in previous two times said
+		thisTurn = said[lastTurn][-1] - said[lastTurn][-2]
+	elif len(said[lastTurn]) == 1:	# If last turn num was the first time it was said, say 0
+		thisTurn = 0
+
+	# add to list 
+	if thisTurn not in said:	# If first time said, need to initialize the key's array
+		said[thisTurn] = [turn]	
+	else:
+		said[thisTurn].append(turn)
+	lastTurn = thisTurn		# Lets me keep iterating
+
+# take n=target turns defined at top:
+for turn in range(target+1):
+	takeTurn(turn)
+	if turn % (target//10) == 0:
+		print(f"Progress: {turn*100//target}%")
+
+print(lastTurn)
